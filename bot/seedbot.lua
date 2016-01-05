c3eab15bc71bf7eadc552ed1e094c19ec61e5690
AliKhodadad <alikhodadad44@gmail.com>
2016-01-05 14:06:12 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {128035701,tonumber(our_id)},--Sudo users
+    sudo_users = {135693512,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     realm = {},--Realms Id
     moderation = {data = 'data/moderation.json'},
