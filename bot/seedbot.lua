00eb72f36d20d50f8dc3ddb13ed261ff77a1c1ae
terinux <aryangithub@gmail.com>
2016-03-14 22:46:17 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -220,7 +220,7 @@ function create_config( )
     "invite",
     "all"
     },
-    sudo_users = {153416374,0,tonumber(our_id)},--Sudo users
+    sudo_users = {115740444,0,tonumber(our_id)},--Sudo users
     disabled_channels = {}
     realm ={},--Realms Id
     moderation = {data = 'data/moderation.json'},
@@ -236,7 +236,7 @@ topkecleon
 Vamptacus
 
 Our channels
-@teleseedch [English]
+@terinux [English]
 ]],
     help_text = [[
 Commands list :
